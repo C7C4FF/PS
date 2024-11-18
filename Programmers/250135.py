da24625d591e6f446c2b19d5bf4481efb08333ee
C7C4FF <80257041+C7C4FF@users.noreply.github.com>
2024-11-19 00:20:03 +0900

    [PCCP 기출문제] 3번 / 아날로그 시계

--- Programmers/250135.py
+++ Programmers/250135.py
@@ -20,20 +20,17 @@ def solution(h1, m1, s1, h2, m2, s2):
         answer += 1
         
     for _ in range(total_seconds):
-        after_sec_hourhand = round(hourhand + (1 / 120), 3)
-        after_sec_minutehand = round(minutehand + (1 / 10), 3)
+        after_sec_hourhand = hourhand + (1 / 120)
+        after_sec_minutehand = minutehand + (1 / 10)
         after_sec_secondhand = secondhand + 6
         
-        # if after_sec_secondhand == 360:
-        #     if int(after_sec_hourhand) == after_sec_secondhand:
-        #         answer -= 1
-        #     if int(after_sec_minutehand) == after_sec_secondhand:
-        #         answer -= 1
-                
         if secondhand < hourhand and after_sec_secondhand > after_sec_hourhand:
+            if minutehand < hourhand and after_sec_minutehand > after_sec_hourhand:
+                answer -= 1
             answer += 1
         if secondhand < minutehand and after_sec_secondhand > after_sec_minutehand:
             answer += 1
+        
         
         hourhand = after_sec_hourhand
         minutehand = after_sec_minutehand
@@ -45,6 +42,8 @@ def solution(h1, m1, s1, h2, m2, s2):
             minutehand -= 360
         if secondhand >= 360:
             secondhand -= 360
-        
     
+    if m2 == 0 and s2 == 0:
+        answer += 1
+        
     return answer
