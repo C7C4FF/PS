2a36169ca76a69d7cf381db1930dfca8c526490d
C7C4FF <80257041+C7C4FF@users.noreply.github.com>
2025-04-02 18:19:47 +0900

    2873. Maximum Value of an Ordered Triplet I

---
+++ LeetCode/2873. Maximum Value of an Ordered Triplet I.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-i/?envType=daily-question&envId=2025-04-02
+# ans = (nums[0] - nums[1]) * nums[2]
+# 결국 답이 되는 가장 큰 곱셈의 결과는 가장 뒤의 인덱스로 결졍되기 떄문에 for 문에서 가장 먼저 업데이트를 해줌.
+
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        largest_value = 0
+        diff = 0
+        ans = 0
+
+        for i in range(len(nums)):
+            ans = max(ans, diff * nums[i])
+            largest_value = max(largest_value, nums[i])
+            diff = max(diff, largest_value - nums[i])
+            
+        return ans if ans >= 0 else 0
