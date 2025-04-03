9a8a58bceeaee78ed27c2a3534231f10844f66fe
C7C4FF <80257041+C7C4FF@users.noreply.github.com>
2025-04-03 11:37:11 +0900

    2874. Maximum Value of an Ordered Triplet II

--- LeetCode/2874. Maximum Value of an Ordered Triplet II.py
+++ LeetCode/2874. Maximum Value of an Ordered Triplet II.py
@@ -1,4 +1,7 @@
-# https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-ii/
+# https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-i/description/?envType=daily-question&envId=2025-04-02
+# 어제 푼 문제가 데일리 문제가 되다
+
+# 2873 은 3 <= nums.length <= 100 이었는데, 2874는 3 <= nums.length <= 10^5 ,,,
 # 요 문제가 O(n) 시간복잡도를 요구하는 듯..
 
 class Solution:
