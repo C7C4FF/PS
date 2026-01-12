aa50e30ebdaa9ad085ddd5a1b9498ef64d49a6eb
C7C4FF <80257041+C7C4FF@users.noreply.github.com>
2026-01-12 19:42:28 +0900

    13. Roman to Integer

--- LeetCode/13. Roman to Integer.py
+++ LeetCode/13. Roman to Integer.py
@@ -25,7 +25,23 @@ class Solution:
         return ans
 
             
+'''
+# 두개씩 번갈아가면서 체크. 뒤가 앞보다 크다면 앞을 빼줌...
+# IV 는 5에서 1을 빼고, IX 는 10에서 1을 빼는 것과 동일......
 
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        symbols = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        ans = 0
+        
+        for a, b in zip(s, s[1:]):
+            if symbols[a] < symbols[b]:
+                ans -= symbols[a]
+            else:
+                ans += symbols[a]
+        
+        return ans + symbols[s[-1]]
+'''
         
 
                 
