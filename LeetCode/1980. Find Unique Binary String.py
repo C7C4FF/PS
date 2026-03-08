8e6decc424f80828bc026289523246049a11dd17
C7C4FF <80257041+C7C4FF@users.noreply.github.com>
2026-03-08 13:54:48 +0900

    1980. Find Unique Binary String

--- LeetCode/1980. Find Unique Binary String.py
+++ LeetCode/1980. Find Unique Binary String.py
@@ -1,3 +1,18 @@
+# https://leetcode.com/problems/find-unique-binary-string/editorial/?envType=daily-question&envId=2026-03-08
+# 칸토어의 대각선 논법 대신 모든 조합을 만들어서 사용하기
+
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        n = len(nums)
+
+        c = list(product([0, 1], repeat = n))
+        
+        for binary in c:
+            target = "".join(map(str, binary))
+            if target not in nums:
+                return target
+                
+'''
 # https://leetcode.com/problems/find-unique-binary-string/?envType=daily-question&envId=2025-02-20
 # n == nums.length.. 현재 주어진 nums의 원소에서 각 자리의 수를 다르게 하면
 # nums 리스트 안의 모든 수에서 하나씩은 차이나게 됨...
@@ -13,3 +28,4 @@ class Solution:
                 answer += '0'
 
         return answer
+'''
