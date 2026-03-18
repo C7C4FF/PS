999e2d6f7d7885f851f1b1bbe1ff25ab954cf3d6
C7C4FF <80257041+C7C4FF@users.noreply.github.com>
2026-03-18 12:14:17 +0900

    3070. Count Submatrices with Top-Left Element and Sum Less Than k

--- LeetCode/3070. Count Submatrices with Top-Left Element and Sum Less Than k.py
+++ LeetCode/3070. Count Submatrices with Top-Left Element and Sum Less Than k.py
@@ -1,7 +1,17 @@
 # https://leetcode.com/problems/count-submatrices-with-top-left-element-and-sum-less-than-k/?envType=daily-question&envId=2026-03-18
 
-# 누적합을 만들면서 k 보다 작거나 같은 경우에 하나씩 세기
+# itertools.accumulate 누적합 구하는 방법으로 grid의 누적합 구하기
+# 누적합으로 바뀐 grid에 zip(*grid) 으로 행렬을 전치시켜 누적합 구하기
+class Solution:
+    def countSubmatrices(self, grid: List[List[int]], k: int) -> int:
+        grid = map(accumulate, grid)
+        grid = map(accumulate, zip(*grid))
+        return sum(val <= k for row in grid for val in row )
+        
 
+---
+# 누적합을 만들면서 k 보다 작거나 같은 경우에 하나씩 세기
+# 0,0 위치가 0 인 경우에는 셀 수 없으니 ans = 1로 시작하기. (1 <= k <= 10**9)
 class Solution:
     def countSubmatrices(self, grid: List[List[int]], k: int) -> int:
         m, n = len(grid), len(grid[0])
