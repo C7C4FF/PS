1d5e7cb9258acd7ff4073f1c886f9021d34a6e2b
C7C4FF <80257041+C7C4FF@users.noreply.github.com>
2026-04-07 20:10:41 +0900

    3001. Minimum Moves to Capture The Queen

---
+++ LeetCode/3001. Minimum Moves to Capture The Queen.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/minimum-moves-to-capture-the-queen/description/
+# 퀸 - 룩 , 퀸 - 비숍 사이에 다른 기물이 존재하는지 확인하기
+# 바로 잡을 수 있거나 2번 걸리거나. 둘 중 하나임
+
+class Solution:
+    def minMovesToCaptureTheQueen(self, a: int, b: int, c: int, d: int, e: int, f: int) -> int:
+        rook = (a, b, 'r')
+        bishop = (c, d, 'b')
+        queen = (e, f, 'q')
+
+        if a == e:
+            if not (c == a and min(b, f) < d < max(b, f)): # 중앙에 비숍이 있는지 확인
+                return 1
+            else:
+                return 2
+        if b == f:
+            if not (d == b and min(a, e) < c < max(a, e)):
+                return 1
+            else:
+                return 2
+
+        if abs(c-e) == abs(d-f):
+            if abs(c-a) == abs(d-b) and abs(e-a) == abs(f-b): # 기울기 부호 확인하기
+                if (abs(c-e) * abs(b-f)) == (abs(a-e) * abs(d-f)): # 전부 같은 기울기를 가지고 있다면
+                    line = [rook, bishop, queen]
+                    line.sort(key=lambda x:x[0])
+                    if (line[0][2] == 'b' and line[-1][2] == 'q') or (line[0][2] == 'q' and line[-1][2] == 'b'): # 퀸가 비숍이 처음과 맨 끝이라면 중간에 룩이 들어가 있음
+                       return 2 
+                    else:
+                        return 1
+            else:
+                return 1
+        else:
+            return 2
+
+        
