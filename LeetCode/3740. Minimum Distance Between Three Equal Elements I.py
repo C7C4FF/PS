e971f175bffd2dc68cd7b791066b9079c33ef01b
C7C4FF <80257041+C7C4FF@users.noreply.github.com>
2026-04-10 11:12:40 +0900

    3740. Minimum Distance Between Three Equal Elements I

--- LeetCode/3740. Minimum Distance Between Three Equal Elements I.py
+++ LeetCode/3740. Minimum Distance Between Three Equal Elements I.py
@@ -1,5 +1,29 @@
 # https://leetcode.com/problems/minimum-distance-between-three-equal-elements-i/description/?envType=daily-question&envId=2026-04-10
 
+# abs(i - j) + abs(j - k) + abs(k - i) 거리는 i > j > k 임
+# 절댓값을 벗기면 j - i + k - j + k - i  -> j가 사라져서 2 * (k - i) 가 됨
+# 가장 최근에 나온 값 3개로 계산하기
+
+class Solution:
+    def minimumDistance(self, nums: List[int]) -> int:
+        ans = float('inf')
+        cnt = defaultdict(list)
+
+        for idx, num in enumerate(nums):
+            cnt[num].append(idx)
+
+            if len(cnt[num]) >= 3:
+                distance = (cnt[num][-1] - cnt[num][-3]) * 2
+                ans = min(ans, distance)
+
+        if ans == float('inf'):
+            return -1
+        
+        return ans
+
+
+'''
+
 class Solution:
     def minimumDistance(self, nums: List[int]) -> int:
         ans = float('inf')
@@ -18,6 +42,9 @@ class Solution:
 
         if ans == float('inf'):
             return -1
-        
+
         return ans
+'''
+        
+        
         
