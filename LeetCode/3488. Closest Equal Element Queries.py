eba151b54306e569ad8e59d75ac3ea5ba36f5175
C7C4FF <80257041+C7C4FF@users.noreply.github.com>
2026-04-16 13:57:09 +0900

    3488. Closest Equal Element Queries

--- LeetCode/3488. Closest Equal Element Queries.py
+++ LeetCode/3488. Closest Equal Element Queries.py
@@ -1,6 +1,41 @@
 # https://leetcode.com/problems/closest-equal-element-queries/description/?envType=daily-question&envId=2026-04-16
 # 리스트 길이가 크다면 이어 붙이기보다는 모듈러 연산하기
+# 몇 번째인지 미리 계산하기 or 이진탐색으로 계산하기
+# 이진탐색은 정렬된 리스트여야 가능하지만, 어차피 순차적으로 넣었기 때문에 정렬되어 있음
 
+class Solution:
+    def solveQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        ans = []
+        n = len(nums)
+
+        seen = defaultdict(list)
+        for i in range(n):
+            seen[nums[i]].append(i)
+
+        for query in queries:
+            target = nums[query]
+            pos = seen[target]
+            m = len(pos)
+
+            if m < 2:
+                ans.append(-1)
+            else:
+                start = bisect.bisect_left(pos, query)
+
+                now = pos[start]
+                next_idx = pos[(start + 1) % m]
+                prev_idx = pos[start - 1]
+
+                distance_to_next = (next_idx - now + n) % n
+                distance_to_prev = (now - prev_idx + n) % n
+
+                distance = min(distance_to_next, distance_to_prev)
+
+                ans.append(distance)
+        
+        return ans
+                
+'''
 class Solution:
     def solveQueries(self, nums: List[int], queries: List[int]) -> List[int]:
         ans = []
@@ -34,3 +69,4 @@ class Solution:
                 ans.append(distance)
         
         return ans
+'''
