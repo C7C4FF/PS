650aa630840d90abc8fce9eaab0f79fde0b2beb4
C7C4FF <80257041+C7C4FF@users.noreply.github.com>
2026-04-20 14:05:15 +0900

    1237. Find Positive Integer Solution for a Given Equation

--- LeetCode/1237. Find Positive Integer Solution for a Given Equation.py
+++ LeetCode/1237. Find Positive Integer Solution for a Given Equation.py
@@ -1,4 +1,6 @@
 # https://leetcode.com/problems/find-positive-integer-solution-for-a-given-equation/description/
+# binary search를 사용할 수 있는 이유: monotoically increasing 이라고 명시되어 있기 때문
+# f(x, y) < f(x+1, y) & f(x, y) < f(x, y+1) : x도 y도 오름차순으로 정렬되어 있다는 뜻
 
 """
    This is the custom function interface.
@@ -14,6 +16,27 @@
 class Solution:
     def findSolution(self, customfunction: 'CustomFunction', z: int) -> List[List[int]]:
         ans = []
+
+        for x in range(1, 1001):
+            low, high = 1, 1000
+
+            while low <= high:
+                mid = (low + high) // 2
+
+                if customfunction.f(x, mid) == z:
+                    ans.append([x, mid])
+                    break
+                elif customfunction.f(x, mid) > z:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+
+        return ans
+
+'''
+class Solution:
+    def findSolution(self, customfunction: 'CustomFunction', z: int) -> List[List[int]]:
+        ans = []
         
         for x in range(1, 1001):
             for y in range(1, 1001):
@@ -21,3 +44,4 @@ class Solution:
                     ans.append([x, y])
 
         return ans
+'''
