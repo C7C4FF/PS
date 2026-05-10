efdb812b1878f1735b4fcad15ce0bb79bc1d2df3
C7C4FF <80257041+C7C4FF@users.noreply.github.com>
2026-05-10 23:25:19 +0900

    2770. Maximum Number of Jumps to Reach the Last Index

---
+++ LeetCode/2770. Maximum Number of Jumps to Reach the Last Index.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/maximum-number-of-jumps-to-reach-the-last-index/description/?envType=daily-question&envId=2026-05-10
+
+class Solution:
+    def maximumJumps(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        dp = [float("-inf")] * n
+        dp[0] = 0
+
+        for i in range(1, n):
+            for j in range(i):
+                if abs(nums[j] - nums[i]) <= target:
+                    dp[i] = max(dp[i], dp[j] + 1)
+
+        return -1 if dp[n - 1] < 0 else dp[n - 1]
