af793803030a769c130acc5bd14e32f9255918bd
C7C4FF <80257041+C7C4FF@users.noreply.github.com>
2026-05-21 16:07:38 +0900

    3043. Find the Length of the Longest Common Prefix.py

--- LeetCode/3043. Find the Length of the Longest Common Prefix.py
+++ LeetCode/3043. Find the Length of the Longest Common Prefix.py
@@ -1,3 +1,29 @@
+# https://leetcode.com/problems/find-the-length-of-the-longest-common-prefix/?envType=daily-question&envId=2026-05-21
+# arr1 에서 가능한 prefix 를 전부 집합에 넣고, arr2 에서 가장 긴 경우를 찾기
+
+class Solution:
+    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+        prefixs = set()
+        for num in arr1:
+            temp = ''
+            for n in str(num):
+                temp += n
+                prefixs.add(temp)
+        
+        ans = 0
+        for num in arr2:
+            prefix = ''
+            for n in str(num):
+                prefix += n
+                if prefix in prefixs:
+                    ans = max(ans, len(prefix)
+                else:
+                    break
+
+        return ans
+
+
+'''
 # https://leetcode.com/problems/find-the-length-of-the-longest-common-prefix/description/
 
 class Solution:
@@ -20,3 +46,4 @@ class Solution:
                     ans = max(ans, len(possible))
 
         return ans
+'''
