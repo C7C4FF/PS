0e06ecfe8195ff6af0939af6ff47d8a3095aa60a
C7C4FF <80257041+C7C4FF@users.noreply.github.com>
2026-07-01 23:55:29 +0900

    2812. Find the Safest Path in a Grid

---
+++ LeetCode/2812. Find the Safest Path in a Grid.py
@@ -0,0 +1,43 @@
+# https://leetcode.com/problems/find-the-safest-path-in-a-grid/?envType=daily-question&envId=2026-07-01
+# https://leetcode.com/problems/find-the-safest-path-in-a-grid/solutions/8368584/solution-by-la_castille-6y31/
+
+class Solution:
+    dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+
+    def maximumSafenessFactor(self, A: List[List[int]]) -> int:
+        if A[0][0] or A[-1][-1]: return 0
+        n, q = len(A), deque()
+
+        for i in range(n):
+            for j in range(n):
+                if A[i][j]:
+                    q.append((i, j))
+
+        while q:
+            i, j = q.popleft()
+            v = A[i][j]
+
+            for dx, dy in self.dirs:
+                x, y = i + dx, j + dy
+
+                if min(x, y) >= 0 and max(x, y) < n and not A[x][y]:
+                    A[x][y] = v + 1
+                    q.append((x, y))
+
+        pq = [(-A[0][0], 0, 0)]
+
+        while pq:
+            sf, i, j = heapq.heappop(pq)
+            sf = -sf
+
+            if i == n - 1 and j == n - 1:
+                return sf - 1
+
+            for dx, dy in self.dirs:
+                x, y = i + dx, j + dy
+
+                if min(x, y) >= 0 and max(x, y) < n and A[x][y] > 0:
+                    heapq.heappush(pq, (-min(sf, A[x][y]), x, y))
+                    A[x][y] *= -1
+
+        return A[n - 1][n - 1] - 1
