cef4a62b6e70863738c5bbaf9feb98be90ee8eeb
C7C4FF <80257041+C7C4FF@users.noreply.github.com>
2026-07-20 15:43:52 +0900

    189. Rotate Array

--- LeetCode/189. Rotate Array.py
+++ LeetCode/189. Rotate Array.py
@@ -1,4 +1,26 @@
-# https://leetcode.com/problems/rotate-array/description/
+# https://leetcode.com/problems/rotate-array/
+# 각 원소당 최대 2번까지 뒤집힐 수 있음. 시간복잡도 O(n)
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        n = len(nums)
+        k %= n
+
+        def reverse(l, r):
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+        
+        reverse(0, n-1)
+        reverse(0, k-1)
+        reverse(k, n-1)
+
+'''
 
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
@@ -10,3 +32,5 @@ class Solution:
             nums.insert(0, this)
 
             k -= 1
+
+'''
